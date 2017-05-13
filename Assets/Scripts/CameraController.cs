82a7ec8e923d922eacf66861726a45577c95608f
Holy-Knight <vnycall74@naver.com>
2017-05-13 20:23:12 +0900

    Concept

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	GameObject rocket;
+
+	Transform rocketPos;
+
+	// Use this for initialization
+	void Start () {
+		rocket = GameObject.Find("Rocket");
+		rocketPos = rocket.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera();
+	}
+
+	void Camera()
+	{
+		transform.position = new Vector3(rocketPos.position.x, rocketPos.position.y, -10f);
+	}
+}
---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+
+	GameManager gameManager;
+	Rigidbody2D rgd2d;
+
+
+	private float h;
+	private float v;
+	
+	private float hspeed = 1f;
+	private float vspeed = 50f;
+
+	Transform trans;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameManager.Instance;
+		rgd2d = gameObject.GetComponent<Rigidbody2D>();
+
+		trans = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Keyboard();
+	}
+
+	private void Keyboard()
+	{
+		if(Input.GetKey(KeyCode.Space))
+		{
+			rgd2d.AddForce(Vector2.up * vspeed);
+		}
+
+		h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        trans.Translate(moveDir.normalized * Time.deltaTime * hspeed  , Space.Self);
+      
+		trans.Rotate(new Vector3(0, 0, -h));
+	}
+}
