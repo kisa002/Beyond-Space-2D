2249c792cb695d44a3afb215307595d748d479df
Holy-Knight <vnycall74@naver.com>
2017-05-13 16:17:28 +0900

    GameManager Init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager instance = null;
+
+	public static GameManager Instance
+	{
+		get
+		{
+			if(instance == null)
+			{
+				instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+				if(instance == null)
+					Debug.LogError("Can not find Find GameManager");
+			}
+
+			return instance;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
